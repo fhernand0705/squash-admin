d65a548d876a37a0e7d4e6f184b5d0bd784117c0
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-02 20:25:43 -0500

    finished re-styling modals...will circle back later

---
+++ app/assets/javascripts/clinics.js
@@ -0,0 +1,9 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+var clinic_modal = document.getElementById('clinic-modal');
+
+function modalOpen() {
+  clinic_modal.style.display = "grid";
+  clinic_modal.style.justifyContent = "center";
+}
---
+++ app/assets/javascripts/coaches.js
@@ -0,0 +1,9 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+var coach_modal = document.getElementById('coach-modal');
+
+function modalOpen() {
+  coach_modal.style.display = "grid";
+  coach_modal.style.justifyContent = "center";
+}
