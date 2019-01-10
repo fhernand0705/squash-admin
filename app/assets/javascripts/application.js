7d0ff00bbbfca2cdf288dabf1e6f148b8c0ff1e5
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-10 14:57:49 -0500

    added assets(local) to public/assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,17 @@
 //= require jquery
 //= reqire jquery_ujs
 
-var cohort_modal = document.getElementById('cohort-modal');
-var side_bar = document.getElementById('side-bar')
-
-function cohortOpen() {
- cohort_modal.style.display = "grid";
- cohort_modal.style.justifyContent = "center";
- side_bar.style.zIndex = "0";
-};
-
-window.onclick = function(event) {
-  if (event.target == cohort_modal) {
-    cohort_modal.style.display = "none";
-  }
-}
+// var cohort_modal = document.getElementById('cohort-modal');
+// var side_bar = document.getElementById('side-bar')
+//
+// function cohortOpen() {
+//  cohort_modal.style.display = "grid";
+//  cohort_modal.style.justifyContent = "center";
+//  side_bar.style.zIndex = "0";
+// };
+//
+// window.onclick = function(event) {
+//   if (event.target == cohort_modal) {
+//     cohort_modal.style.display = "none";
+//   }
+// }
