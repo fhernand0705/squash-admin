8284bcf847c310da96f61c4ca60bcbdf512821ee
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-09-24 09:21:38 -0400

    refactoring demo section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    if @user.id === 7
+    if @user.id === 1
       session[:user_id] = @user.id
     end
   end
